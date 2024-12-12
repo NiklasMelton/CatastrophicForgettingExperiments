e4ec3770daa0236ad8af3706492539f9121c5e6a
Sasha Petrenko <sap625@umsystem.edu>
2024-12-12 15:14:20 -0600

    Init pyproject, julia files

---
+++ README.md
@@ -0,0 +1,11 @@
+# CatastrophicForgettingExperiments
+
+TODO
+
+## Installation
+
+```shell
+mamba create -n cfar-exp python=3.11
+mamba activate cfar-exp
+pip install -e .[dev]
+```
---
+++ src/CatastrophicForgettingExperiments.jl
@@ -0,0 +1,10 @@
+module CatastrophicForgettingExperiments
+
+using
+    AdaptiveResonance,
+    DrWatson,
+    Plots
+
+greet() = print("Hello World!")
+
+end # module CatastrophicForgettingExperiments
